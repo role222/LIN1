a4d69cfc5c432bf8cfb78a60db2b7ac0bb751e4c
role222 <role222oka@gmail.com>
2026-09-22 11:44:30 +0300

    so this is work, but it's not full

--- main.py
+++ main.py
@@ -19,21 +19,18 @@ def Gaus_Jordan(A, B):
             C_ = C[i].copy()
             C[i] = C[max_row_index].copy()
             C[max_row_index] = C_.copy()
-    for x in range(0, 3):
-    #x = 0
+    print(C)
+    for x in range(0, C.shape[0]):
         for y in range(x, C.shape[0]):
             C[y] = C[y] / C[y, x]
-        print(C)
         for y in range(x+1, C.shape[0]):
             for z in range(0, C.shape[1]):
                 C[y, z] = C[y, z] - C[x, z]
-
-
     print(C)
 
 
 
-A,B = read_file("equ2.txt")
+A,B = read_file("equ3.txt")
 
 Te = np.linalg.det(A)
 #print(Te)

91541b69e0d814f988b875bda50d2e3613e31b98
role222 <role222oka@gmail.com>
2026-09-23 09:44:42 +0300

    Now method Gaus-Jordan work corectly  # Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -19,18 +19,23 @@ def Gaus_Jordan(A, B):
             C_ = C[i].copy()
             C[i] = C[max_row_index].copy()
             C[max_row_index] = C_.copy()
-    print(C)
     for x in range(0, C.shape[0]):
         for y in range(x, C.shape[0]):
             C[y] = C[y] / C[y, x]
         for y in range(x+1, C.shape[0]):
             for z in range(0, C.shape[1]):
                 C[y, z] = C[y, z] - C[x, z]
-    print(C)
+    X = np.zeros_like(B1)
+    for x_ in range(0, C.shape[0]):
+        x = C.shape[0] - x_ - 1
+        for y in range(x+1, C.shape[1]-1):
+            C[x, C.shape[1] - 1] = C[x, C.shape[1] - 1] - (X[y, 0] * C[x, y])
+        X[x, 0] = C[x, C.shape[1] - 1]
+    print(X)
 
 
 
-A,B = read_file("equ3.txt")
+A,B = read_file("equ1.txt")
 
 Te = np.linalg.det(A)
 #print(Te)
